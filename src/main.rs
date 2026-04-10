08b0dd8219105d90dfbfba7a04660ea2c55cb40a
Zackeus Zetterberg <zackeua@gmail.com>
2026-04-11 00:12:00 +0200

    Move player into separate file

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use macroquad::prelude::*;
 use macroquad::rand::srand;
 
 
-use crate::game::{Powerup, PowerupType};
+use crate::game::{Player, Powerup, PowerupType};
 
 mod game;
 // ================== CONSTANTS ==================
@@ -36,100 +36,6 @@ const COLOR_PALETTE: &[(f32, f32, f32); 25] = &[
 ];
 
 // ================== GAME CORE ==================
-
-#[derive(Clone)]
-struct Player {
-    pos: Vec2,
-    dir: f32,
-    trail: Vec<Option<Vec2>>,
-
-    hole_timer: f32,
-    hole_cooldown: f32,
-    in_hole: bool,
-
-    speed_multiplier: f32,
-    turn_multiplier: f32,
-    effect_timer: f32,
-    trail_thickness: f32,
-}
-
-impl Player {
-    fn new(pos: Vec2, dir: f32) -> Self {
-        Self {
-            pos,
-            dir,
-            trail: vec![Some(pos)],
-            hole_timer: 0.0,
-            hole_cooldown: rand::gen_range(1.5, 3.0),
-            in_hole: false,
-            speed_multiplier: 1.0,
-            turn_multiplier: 1.0,
-            effect_timer: 0.0,
-            trail_thickness: 3.0,
-        }
-    }
-
-    fn get_direction_vector(&self) -> Vec2 {
-        vec2(self.dir.cos(), self.dir.sin())
-    }
-
-    fn update(&mut self, dt: f32, turn: f32, config: &GameConfig) {
-
-        self.dir += turn * config.turn_speed * dt;
-        self.hole_timer += dt;
-
-        let velocity = self.get_direction_vector() * config.speed * dt;
-        self.pos += velocity * self.speed_multiplier;
-
-        if self.in_hole && self.hole_timer > config.hole_duration {
-            self.in_hole = false;
-            self.hole_timer = 0.0;
-            self.hole_cooldown = rand::gen_range(config.hole_interval_min, config.hole_interval_max);
-            
-        } else if !self.in_hole && self.hole_timer > self.hole_cooldown {
-            self.in_hole = true;
-            self.hole_timer = 0.0;
-        }
-
-        let last = self.trail.iter().rev().find_map(|&p| p);
-
-        let step_distance = TRAIL_STEP * config.speed / SPEED; // Adjust step based on speed config
-        if let Some(last_pos) = last {
-            if last_pos.distance(self.pos) > step_distance {
-                if self.in_hole {
-                    // Only insert one None (avoid spam)
-                    if !matches!(self.trail.last(), Some(None)) {
-                        self.trail.push(None);
-                    }
-                } else {
-                    self.trail.push(Some(self.pos));
-                }
-            }
-        }
-
-        if self.effect_timer > 0.0 {
-            self.effect_timer -= dt;
-            if self.effect_timer <= 0.0 {
-                self.speed_multiplier = 1.0;
-                self.turn_multiplier = 1.0;
-                self.trail_thickness = 3.0;
-            }
-        }
-    }
-
-    fn reset(&mut self, pos: Vec2, dir: f32) {
-        self.pos = pos;
-        self.dir = dir;
-        self.trail.clear();
-        self.trail.push(Some(pos));
-        self.hole_timer = 0.0;
-        self.in_hole = false;
-        self.speed_multiplier = 1.0;
-        self.turn_multiplier = 1.0;
-        self.effect_timer = 0.0;
-        self.trail_thickness = 3.0;
-    }
-}
 
 fn apply_powerup(player_idx: usize, kind: PowerupType, players: &mut [Player], _config: &mut GameConfig) {
     match kind {
@@ -178,7 +84,7 @@ enum RoundState {
 }
 
 #[derive(Clone)]
-struct GameConfig {
+pub struct GameConfig {
     speed: f32,
     turn_speed: f32,
     hole_interval_min: f32,
